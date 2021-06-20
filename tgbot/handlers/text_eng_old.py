63bc5e06963429360d252e8b5facb08a4c2992e1
Waldemar Antypov <waldemar.antypov@gmail.com>
2021-06-20 17:32:42 +0300

    new text

--- tgbot/handlers/text_eng_old.py
+++ tgbot/handlers/text_eng_old.py
@@ -1,13 +1,13 @@
 # command start
 start_created = "Hello, {first_name}!\n\n"
-start_created_portfolio = '/portfolio to configure your portfolio - \n\n'
+start_created_portfolio = 'To Configure your portfolio - /portfolio\n\n'
 start_created_feedback = 'If you will have a minute to answer 5 questions - press /feedback\n' \
                          'Your answers will help us to make the bot more useful.\n\n'
 start_created_portfolio_example = "This is how the portfolio will look like:"
 start_created_share = 'You can simply share the bot by sending this message: {share_url}'
 start_created_share_command = 'You can get your unique link to share it with your friends here:' \
                               ' /share'
-start_change_language = '/language to change a bot language: '
+start_change_language = 'To Сhange bot language: /language'
 start_created_inbaze = 'Ещё у нас есть телеграм канал, где рассказываем про финансовую грамотность: @inbaze'
 
 # OKAY
@@ -19,12 +19,12 @@ feedback = "Fill out the Google Form survey, so I can understand how useful I am
 share_command = "You can share the bot by sending the message below:\n\n"
 share_message = "Hi!\n\n" \
                 "My name is Inbaze and I am a portfolio-bot 🤖\n" \
-                "I help to track the UPs 📈 & DOWNs 📉 of your investment portfolio (stocks and cryptocurrencies).\n\n" \
-                "The bot is completely free, so welcome!\n" \
+                "I help to track the UPs 📈 / DOWNs 📉 of your investment portfolio (stocks and cryptocurrencies).\n\n" \
+                "The bot is completely free. Welcome!\n" \
                 "{share_url}"
 
 # command portfolio
-wait_for_portfolio = 'In the process ⏱\n\n' # \
+wait_for_portfolio = 'Have a patience - this may take a while ⏱\n\n' # \
                      # 'The larger the portfolio - the more information to collect on it 🤓\n'
 # OKAY
 portfolio_head = 'Portfolio:\n'
@@ -37,28 +37,28 @@ efficiency_button = '%'
 amount_button = 'Amount'
 update_button = '💸 Update portfolio'
 
-wait_update = 'In the process ⏱'
+wait_update = 'The portfolio is being updated - it may take a couple of minutes ⏱'
 
 modify_button = '💼 Modify»'
 modify_question = 'What do you want to do?'
 
 balance_button = '✏️ Modify balances»'
-add_stock_button = '➕ Add a new asset»'
-delete_stock_button = '➖ Delete an asset»'
+add_stock_button = '➕ Add new asset»'
+delete_stock_button = '➖ Delete asset»'
 close_button = '❌ Close'
 close_modify_menu_portfolio = 'Good.\n\n' \
                               'If you want to take another look at the portfolio - press /portfolio.\n' \
-                              'If you want to leave the feedback - press /feedback.\n' \
-                              'If you want to share the bot with your friends - press /share.'
+                              'If you want to leave feedback - press /feedback\n' \
+                              'If you want to share the bot with your friends - press /share'
 # OKAY
 
-change_balance_text = 'Enter the symbol of the asset that you want to CHANGE ✏️\n\n' \
+change_balance_text = 'Enter the symbol of the stock / cryptocoin that you want to CHANGE ✏️\n\n' \
                       '<i>(Tesla has a symbol - TSLA, and bitcoin - "BTC-USD")</i>.'
-add_stock_text = 'Enter the symbol of the asset that you want to ADD ➕\n\n' \
+add_stock_text = 'Enter the symbol of the stock / cryptocoin that you want to ADD ➕\n\n' \
                  '<i>(Tesla has a symbol - TSLA, and bitcoin - "BTC-USD")</i>.\n\n' \
                  'p.s. If you are not sure about the asset\'s ticker' \
                  ' - you can check it here: https://finance.yahoo.com'
-delete_stock_text = 'Enter the symbol of the asset that you want to DELETE ❌\n\n' \
+delete_stock_text = 'Enter the symbol of the stock / cryptocoin that you want to DELETE ❌\n\n' \
                     '<i>(Tesla has a symbol - TSLA, and bitcoin - "BTC-USD")</i>.' \
     # okay
 
@@ -82,14 +82,14 @@ wrong_text_entered = "Sorry, I didn\'t understand: '{text}'"
 # okay
 
 modify_balance_stock_not_in_portfolio = 'Error: this position is not included into portfolio, or this ticker does not exist.\n\n' \
-                                        '<i>In the first case you should add a ticker via "➕ Add a new asset</i>"\n' \
+                                        '<i>In the first case you should add a ticker via "➕ Add new asset</i>"\n' \
                                         '<i>In the second case you should try to find ticker here https://finance.yahoo.com</i>'
 modify_balance_stock_symbol_shares = 'Currently you have <b><i>{shares}</i></b> shares of <b><i>{symbol}</i></b>\n' \
                                      'What would be the new balance of {symbol}?\n\n' \
                                      '<i>(For example: 2  - "2",2,53 - "2.53")</i> \n\n'
 delete_stock_info = 'Currently you have <b><i>{shares}</i></b> shares / crypto coins of <b><i>{symbol}</i></b>.\n' \
                     'Do you want to remove this asset from your portfolio?'
-delete_stock_success = 'You have removed <b><i>{symbol}</i></b> from the portfolio.'
+delete_stock_success = 'You have removed <b><i>{symbol}</i></b> from the portfolio, I hope you closed it profitably 🙏'
 delete_stock_cancel = "We won't remove <b><i>{symbol}</i></b>, okay"
 delete_stock_yes_text = 'Delete 🗑'
 delete_stock_no_text = 'Save ❤️'
@@ -107,15 +107,15 @@ change_to_invest_text = 'Your current balance is: <b><i>{to_invest}</i></b>\n\n'
 changed_to_invest_text = 'Great! I have updated your balance and now it is: <b><i>{to_invest}</i></b>\n'
 
 add_stock_symbol_check_already_have = 'You already have <b><i>{shares}</i></b> shares / crypto coins of <b><i>({symbol})</i></b>.\n\n' \
-                                      'If you want to change the balance of the stock - choose "✏️ Edit an asset"' \
+                                      'If you want to change the balance of the stock - choose "✏️ Edit asset"' \
                                       ' in /portfolio).'
 add_stock_symbol_check_success = '👍 <b><i>{symbol}</i></b> was added into portfolio.\n\n' \
                                  'How much <b><i>{symbol}</i></b> do you want to add?\n\n' \
                                  '<i>(Example: 2 - "2", 2,53 - "2.53")</i>\n\n'
-add_stock_not_in_yfinance = 'Error: You entered the asset symbol incorrectly.\n\n' \
+add_stock_not_in_yfinance = 'Error: You entered the stock / crypto coin symbol incorrectly.\n\n' \
                             '<i>See the correct spelling of the symbol here https://finance.yahoo.com</i>'
 
-language_ask = 'Choose a language:'
+language_ask = 'Choose language:'
 language_EN = "🇺🇸"
 language_RU = "🇷🇺"
 language_modefied_succes = "Language changed successfully!"
